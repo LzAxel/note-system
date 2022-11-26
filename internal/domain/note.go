1213e0c4170b901b54c726e405ed750123f0b22a
lzaxel <lzaxelmail@gmail.com>
2022-11-26 23:23:40 +0200

    modify: note, account models

--- internal/domain/note.go
+++ internal/domain/note.go
@@ -1,11 +1,21 @@
 package domain
 
 type Note struct {
-	Name      string
-	Text      string
-	Tag       string
-	Url       string
-	IsPublic  bool
-	CreatedAt int
-	UpdatedAt int
+	Id        int    `db:"id"`
+	Name      string `json:"name" db:"name"`
+	Text      string `json:"text" db:"text"`
+	Tag       string `json:"tag" db:"tag"`
+	Url       string `json:"url" db:"url"`
+	IsPublic  bool   `json:"isPublic" db:"is_public"`
+	CreatedAt int    `json:"createdAt" db:"created_at"`
+	UpdatedAt int    `json:"updatedAt" db:"updated_at"`
+	AccountId int    `json:"-" db:"account_id"`
+}
+
+type CreateNoteDTO struct {
+	Name      string `json:"name" binding:"required"`
+	Text      string `json:"text" binding:"required"`
+	Tag       string `json:"tag"`
+	IsPublic  bool   `json:"isPublic"`
+	AccountId int    `json:"-"`
 }
--- internal/domain/user.go
+++ internal/domain/user.go
@@ -1,7 +1,21 @@
 package domain
 
-type User struct {
-	Name         string
-	PasswordHash string
-	CreatedAt    int
+import "time"
+
+type Account struct {
+	Id           int       `db:"id"`
+	Username     string    `db:"username"`
+	PasswordHash string    `db:"password_hash"`
+	HashSalt     string    `db:"hash_salt"`
+	CreatedAt    time.Time `db:"created_at"`
+}
+
+type CreateAccountDTO struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
+type LoginAccountDTO struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
 }
